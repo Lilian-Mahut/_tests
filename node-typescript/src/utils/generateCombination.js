5c7a958703fe0ba208922b4b4f89001e1bcecc3f
Lilian Mahut <lilian.mahut@pm.me>
2024-01-25 14:43:35 +0100

    renommage du fichier principal ajout du dockerfile ajout de prettier & cfg correction du script de test & start au package.json ajout du script start modification du paquet Babel/cli en dependancies > devDependancies

--- node-typescript/src/utils/generateCombination.js
+++ node-typescript/src/utils/generateCombination.js
@@ -1,17 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function generateCombination(array) {
-  const result = [];
-
-  function generateRecursively(target, startIndex) {
-    if (target !== "") {
-      result.push(target);
+    const result = [];
+    function generateRecursively(target, startIndex) {
+        if (target !== "") {
+            result.push(target);
+        }
+        for (let i = startIndex; i < array.length; i++) {
+            generateRecursively(target + array[i], i + 1);
+        }
     }
-    for (let i = startIndex; i < array.length; i++) {
-      generateRecursively(target + array[i], i + 1);
-    }
-  }
-  generateRecursively("", 0);
-  return result;
+    generateRecursively("", 0);
+    return result;
 }
 exports.default = generateCombination;
